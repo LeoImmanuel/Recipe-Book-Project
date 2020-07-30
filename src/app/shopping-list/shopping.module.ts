a328ad3d77d0b55bd039f1b909a9f9b4b080d8c5
LeoImmanuel <leoimmanuelj@gmail.com>
2020-07-30 12:38:04 +0530

    Optimizing & Lazy Loading

---
+++ src/app/shopping-list/shopping.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingRoutingModule } from './shopping-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [ RouterModule, SharedModule, FormsModule, ShoppingRoutingModule ]
+})
+export class ShoppingModule {
+
+}
